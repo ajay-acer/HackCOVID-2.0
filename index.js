68128b10b72043ad165f96b33c2c8f784cdc9065
adarshjp <adarsh.24padmashali@gmail.com>
2021-06-17 10:02:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express')
+      app=express()
+      bodyparser=require('body-parser')
+      require('dotenv').config()
+
+app.set("view engine","ejs")
+app.use(express.static("public"))
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.get("/",(req,res)=>{
+    res.send("Login Page")
+})
+
+app.listen(process.env.PORT||3000,()=>{
+    console.log("Server started at port 3000")
+})
